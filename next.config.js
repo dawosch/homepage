54fc73c32295433fb07972ef3c4dd1b785b30a21
David <david.spanier@fincon.eu>
2022-04-28 11:31:31 +0200

    gh-pages setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isDevelopment = process.env.NODE_ENV === 'development';
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isDevelopment ? '/homepage/' : '',
 };
 
 module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
+  console.log(process.env.NODE_ENV);
+  
   return (
     <div className={styles.container}>
       <Head>
